c5cf649e11bad349530fe68f18355ff8e78a581b
Nicole Page <npage1129@gmail.com>
2022-12-04 11:16:44 -0700

    navbar layout

--- src/header.js
+++ src/header.js
@@ -3,25 +3,25 @@ import './header.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Stack from 'react-bootstrap/Stack';
 
 const Header = () => {
 
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar id="nav" bg="light" expand="lg">
+            
         <Container>
-            <div id="titler">
+        <Stack gap={2}>
                 <Navbar.Brand href="/">Lauri's Notary Services</Navbar.Brand>
-            </div>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+             
+            <Nav>
                 <Nav.Link id="about" href="about">About</Nav.Link>
                 <Nav.Link id="services" href="services">Services</Nav.Link>
                 <Nav.Link id="contact" href="contact">Contact</Nav.Link>
             </Nav>
-            </Navbar.Collapse>
+            </Stack>
         </Container>
+        
         </Navbar>  
     );
 }
