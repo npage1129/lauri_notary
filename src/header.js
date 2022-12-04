77903b335796e60e5940f3b4002f1ebbd377cc9f
Nicole Page <npage1129@gmail.com>
2022-12-04 11:46:44 -0700

    nav font

--- src/header.js
+++ src/header.js
@@ -8,21 +8,18 @@ import Stack from 'react-bootstrap/Stack';
 const Header = () => {
 
     return(
-        <Navbar id="nav" bg="light" expand="lg">
-            
+        <Navbar id="nav" bg="light" expand="lg"> 
         <Container>
         <Stack gap={2}>
-                <Navbar.Brand href="/">Lauri's Notary Services</Navbar.Brand>
-             
+                <Navbar.Brand href="/">LAURI'S NOTARY SERVICES</Navbar.Brand>
             <Nav>
-                <Nav.Link id="about" href="about">About</Nav.Link>
-                <Nav.Link id="services" href="services">Services</Nav.Link>
-                <Nav.Link id="contact" href="contact">Contact</Nav.Link>
+                <Nav.Link id="about" href="about">ABOUT</Nav.Link>
+                <Nav.Link id="services" href="services">SERVICES</Nav.Link>
+                <Nav.Link id="contact" href="contact">CONTACT</Nav.Link>
             </Nav>
             </Stack>
-        </Container>
-        
-        </Navbar>  
+        </Container>  
+        </Navbar> 
     );
 }
 
