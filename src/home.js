b11dc8f7eea543bec119db7f57dccf7c6f96fe60
Nicole Page <npage1129@gmail.com>
2022-12-23 18:59:20 -0700

    more

--- src/home.js
+++ src/home.js
@@ -4,7 +4,9 @@ import './home.css';
 import Header from './header';
 import notarystamp from './notarystamp.png';
 import spill from './inkSpill.jpeg';
-import fadepic from './fadepic.jpeg'
+import fadepic from './fadepic.jpeg';
+import pen from './pen.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = ()=>{
     const url = 'https://www.facebook.com/Laurisnotaryservices/';
@@ -12,17 +14,39 @@ const Home = ()=>{
         <div>
         <div id='column'>
             <h1><Header/></h1>
-            <h1 id='snippet'>Lauri Page is a South Florida-based<br></br> Mobile Notary, specializing in all loan types. </h1>
-            <h2 id="subHeader">She is currently available for hire</h2>
+            <h1 id='snippet'>Lauri Page is a South West Florida-based<br></br> Mobile Notary, specializing in all loan types. </h1>
+            <h2 id="subHeader">Available for all of your notary needs</h2>
             <button id="touch">Get in Touch</button>
             </div>
-            <div id="column">
-            <img id="spilll" src={spill}></img>
-            <img id="lauri" src={fadepic}></img>
-            <img id="stamp" src={notarystamp}></img>
-
+            <div id="column" style={{ height: "1500px" }}>
+            <img id="pen" src={pen}></img>
+            <div id ="me-section">
+            <div class="row align-items-start">
+            <div class="col">
+           <img id="lauri" src={fadepic}></img>
+           </div>
+           <div class="col">
+            <h1 id="meHeader"> Putting My Experience to Work</h1>
+            
+            <div class="col">
+            <p id="about-me"> 
+Hi! I'm Lauri and I'm a mobile notary public located in Southwest Florida. I've worked in business development for over 30 years and I've been in the title business for 6 of these, 5 of which I have been a licensed title agent.
+I've completed well-over 1000 closings, and during the process I fell in love with helping people make their dreams become a reality.
+I have a passion for this crazy real estate business and I love working with people and assisting them with their life-changing adventures.My favorite part has always been sitting at the table during the final closings,
+            </p>
             </div>
-        </div>
+            </div>
+            <div class="col">
+            <p id="cont">so I decided to open up my own mobile notary company, which gives me the opportunity to work with everyone.
+            I have focused mainly on all types of loan closings, including refinances and reverse mortgages. 
+            I also love working with sellers during the final steps of their process as well. 
+            I pride myself on being very organized, friendly, and dedicated. 
+            I understand that every client is unique, and I ensure that each job will be tailored to their individual needs.</p>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
     )
 }
 
